8e2cf6472501c6527c01d4b6807330c11d80508f
ericatevissen <ericatev@proton.me>
2023-12-14 17:56:52 +0000

    added types to the pokemon cardsand modified UI to fit them

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,15 +4,45 @@ function Grid({ pokemonList, search, type }: GridProps) {
     return (
         <div className="grid"> 
             {pokemonList.map(pokemon => {
+                let show;
+
                 if (pokemon.name.includes(search.toLowerCase()) && 
                 (type === pokemon.types[0].type.name ||  type === "all" ||
                 (pokemon.types[1] && type === pokemon.types[1].type.name))
-                ) {
+                ) { 
+                    show = true;
+                }
+
+                if (show === true && pokemon.types[1]) {
                     return (
                         <div className="pokemon" key={pokemon.id}>
-                            <p>#{pokemon.id}</p>
-                            <img src={pokemon.sprites.other["official-artwork"].front_default}></img>
-                            <h2>{pokemon.name}</h2>
+                            <div className="top-info">
+                                <h2>{pokemon.name}</h2>
+                                <p>#{pokemon.id}</p>
+                            </div>
+                            <div className="bottom-info">
+                                <div>
+                                    <p>{pokemon.types[0].type.name}</p>
+                                    <p>{pokemon.types[1].type.name}</p>
+                                </div>
+                                <img src={pokemon.sprites.other["official-artwork"].front_default}></img>
+                            </div>
+                        </div>
+                    )
+                }
+                if (show === true) {
+                    return (
+                        <div className="pokemon" key={pokemon.id}>
+                            <div className="top-info">
+                                <h2>{pokemon.name}</h2>
+                                <p>#{pokemon.id}</p>
+                            </div>
+                            <div className="bottom-info">
+                                <div>
+                                    <p>{pokemon.types[0].type.name}</p>
+                                </div>
+                                <img src={pokemon.sprites.other["official-artwork"].front_default}></img>
+                            </div>
                         </div>
                     )
                 }
